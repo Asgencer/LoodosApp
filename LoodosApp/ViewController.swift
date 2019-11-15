c6614e250b3d012a26182d9885db81072c0c01e9
Semih Gençer <asgencersemih@gmail.com>
2019-11-15 20:56:05 +0300

    Initial Commit

---
+++ LoodosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoodosApp
+//
+//  Created by Semih Gençer on 15.11.2019.
+//  Copyright © 2019 Semih Gençer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
